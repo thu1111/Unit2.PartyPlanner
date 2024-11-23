3874ba2e9ae7ce487259bdc7472684256395c53d
Thu Yein Tun <yon.fu111@gmail.com>
2024-11-23 16:42:37 -0500

    checks and update

--- index.js
+++ index.js
@@ -3,56 +3,57 @@ const COHORT = "2410-FTB-ET-WEB-FT";
 const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
 
 const state = {
-  parties: [ ],
-}
+  parties: [],
+};
 
-//2. super render
+//2. Super Render
 async function render() {
   await getParties();
-  renderParties(); 
-} 
+  renderParties();
+}
 render();
 
-//3. get data - update state with parties from /*API*/
 async function getParties() {
   try {
-    const response = await fetch (API_URL);
+    const response = await fetch(API_URL);
     if (!response.ok) {
-      throw new Error("Failed to fetch parties!");
+      throw new Error("Cannot get parties");
     }
 
     const json = await response.json();
     state.parties = json.data;
+    //console.log(json.data);
   } catch (error) {
-    console.log(error);    
+    console.error(error);
   }
 }
 
-//4. render
 function renderParties() {
-  const partiesList = document.querySelector("#parties") //DOM link with ul
+  const partiesList = document.querySelector("#parties");
 
   if (!state.parties.length) {
-    partiesList.innerHTML = "<li>No parties</li>";
+    partiesList.innerHTML = "<li>There is no party!</li>";
     return;
   }
 
-  const partyCards = state.parties.map((party)=>{
+  const partyCards = state.parties.map((party) => {
     const card = document.createElement("li");
     card.classList.add("party");
-    card.innerHTML=`
-    <p>Name: ${party.name}</p>
-    <p>Date: ${party.date}</p>
-    <p>Location: ${party.location}</p>
-    <p>Description: ${party.description}</p>
+    card.innerHTML = `
+      <p>Name: ${party.name}</p>
+      <p>Date: ${party.date}</p>
+      <p>Location: ${party.location}</p>
+      <p>Description: ${party.description}</p>
     `;
 
     const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("deleteBtnClass");
-    deleteBtn.textContent = "Delete Party";
+    deleteBtn.classList.add("deleteBtn");
+    deleteBtn.textContent = "Delete party";
     card.append(deleteBtn);
 
-    deleteBtn.addEventListener("click", ()=>deleteParty(party.id))
+    deleteBtn.addEventListener("click", () => {
+      deleteParty(party.id);
+    });
 
     return card;
   });
@@ -60,11 +61,10 @@ function renderParties() {
   partiesList.replaceChildren(...partyCards);
 }
 
-//5. Add and Create Party
-//Handle form submission for adding a party
+//3. Form Submit, Add and Create
 const form = document.querySelector("form");
 form.addEventListener("submit", addParty);
-//addParty - collect submitted data into an object and create party
+
 async function addParty(event) {
   event.preventDefault();
 
@@ -73,22 +73,24 @@ async function addParty(event) {
     date: new Date(form.partyDate.value).toISOString(),
     location: form.partyLocation.value,
     description: form.partyDescription.value,
-  }
+  };
+  //console.log(partyInfo);
 
   form.reset(); //clear form after submit
-  // console.log(partyInfo);
-  
+
   await createParty(partyInfo);
 }
 
-//createParty - Ask API to create a new party and rerender /*API*/
 async function createParty(partyInfo) {
   try {
     const response = await fetch(API_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(partyInfo),
-    }); 
+    });
+    if (!response.ok) {
+      throw new Error("Cannot create party!");
+    }
     const json = await response.json();
     // console.log(json);
     render();
@@ -97,16 +99,34 @@ async function createParty(partyInfo) {
   }
 }
 
-
-//6. deleteParty - Ask API to delete a party and rerender /*API*/
+//4. Delete
 async function deleteParty(id) {
   try {
     const response = await fetch(`${API_URL}/${id}`, {
       method: "DELETE",
     });
     if (!response.ok) {
-      throw new Error("Party could not be deleted");
+      throw new Error("Party can't be deleted!");
     }
+    render();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//5. Update
+async function updateParty(id, partyInfo) {
+  try {
+    const response = await fetch(`${API_URL}/${id}`, {
+      method: "PUT",
+      headers: { "Contnet-Type": "application/json" },
+      body: JSON.stringify(partyInfo),
+    });
+    if (!response.ok) {
+      throw new Error("Party can't be updated!");
+    }
+    const json = await response.json();
+    // console.log(json);
 
     render();
   } catch (error) {
